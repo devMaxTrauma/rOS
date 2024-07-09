36865f0dde9abc9619b9bfc24105546e6397b9ae
techno.sexking <techno.sexking@icloud.com>
2024-07-09 20:18:56 +0900

    demonslayer.8

--- RKernel.py
+++ RKernel.py
@@ -674,7 +674,7 @@ class RBluetooth:
                 if self.client_sock:
                     print("connected.")
                     self.bluetooth_connected = True
-                    break
+                    # break
                 pass
             except Exception as e:
                 if self.bluetooth_rx_thread is not None:
