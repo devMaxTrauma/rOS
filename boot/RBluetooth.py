c24d64366931e0011c4a7deabc78cce8d5a742d0
techno.sexking <techno.sexking@icloud.com>
2024-07-11 16:26:14 +0900

    EdgeRunner.0.beta2.debug1

--- boot/RBluetooth.py
+++ boot/RBluetooth.py
@@ -63,9 +63,10 @@ def bluetooth_rx_interrupt():
     global bluetooth_connected
     global client_sock
     global callback
+    print("here1")
     try:
         while bluetooth_connected:
-
+            print("here2")
             data = client_sock.recv(1024)
             if not data:
                 break
